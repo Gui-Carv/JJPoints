36fbef28b001de0fb16e371144abc50c38cdf2c1
Gui-Carv <gui.20031603@gmail.com>
2023-07-26 14:28:25 -0300

    timer add

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("teste");
+function updateTimeValue(value) {
+    document.getElementById("timeValue").textContent = value;
+}
+
+function iniciarLuta() {
+    // Capturando os valores dos campos de input
+    const user1 = document.getElementById("user1").value.trim();
+    const user2 = document.getElementById("user2").value.trim();
+
+    // Verificando se os campos de nome foram preenchidos
+    if (user1 === "" || user2 === "") {
+        alert("Por favor, preencha os nomes dos lutadores.");
+        return;
+    }
+
+    // Capturando o tempo de luta
+    const tempoDeLuta = document.getElementById("time").value;
+
+    // Redirecionando para a página luta.html passando os parâmetros na URL
+    window.location.href = `timer.html?user1=${user1}&user2=${user2}&tempo=${tempoDeLuta}`;
+}
